0658b50444deb52de39e26fdf7adc62654a83da7
Alessandro Repola <a.repola5@studenti.unisannio.it>
2025-06-12 12:36:48 +0200

    Ripristino boss, prima nav mesh

--- Assets/myStuff/Scripts/GenericChar/ScriptableObj/CharStats.cs
+++ Assets/myStuff/Scripts/GenericChar/ScriptableObj/CharStats.cs
@@ -5,43 +5,17 @@ using UnityEngine;
 [Serializable]
 public class CharStats : AbstractStatsClass
 {
-    [SerializeField] private EnemyDropper dropper;
-    [SerializeField] private Ragdoller ragdoller;
-    [SerializeField] private Player player;
-
+    [SerializeField] private bool isPlayer = false; 
     protected new void Awake()
     {
         base.Awake();
 
         // money e keys feature trattate in modo speciale, solo a charstats è consentito averle
-        if (player)
+        if (isPlayer)
         {
             features.Add(100, new Feature(FeatureType.money, 100, typeof(int)));
             features.Add(101, new Feature(FeatureType.keys, 100, typeof(int)));
             features.Add(102, new Feature(FeatureType.astroCredits, PlayerPrefs.GetFloat("astroCredits", 0), typeof(int)));
         }
     }
-
-    new void Update()
-    {
-        base.Update();
-
-        var hp = GetFeatureValuesByType<float>(FeatureType.health).Sum();
-        hp = Math.Clamp(hp, 0, GetFeatureValuesByType<float>(FeatureType.maxHealth).Sum());
-
-        if (hp <= 0)
-        {
-            if (player)
-            {
-                ragdoller.Ragdolling(true);
-                player.EmitEvent(EventRegistry.OBJECT_DISABLED, new object[] { gameObject });
-                return;
-            }
-
-            this.gameObject.SetActive(false);
-            dropper.DropItem();
-        }
-
-    }
-
 }
